9ac5917b7b281fc462acba1eaa3e93e93495200f
Tolik9924 <sakurasoft9924@gmail.com>
2024-10-18 02:08:55 +0300

    Make Create Test

--- app/(root)/create-test/page.tsx
+++ app/(root)/create-test/page.tsx
@@ -2,14 +2,20 @@
 
 import { useState } from 'react';
 
-import { Input } from 'core_ui_design_system';
+import { Button, Checkbox, Input } from 'core_ui_design_system';
 
 import styles from './styles.module.css';
+
+interface SubQuestion {
+  id: number;
+  name: string;
+  isCorrect: boolean;
+}
 
 interface Question {
   id: number;
   name: string;
-  subQuestions: string[]
+  subQuestions: Array<SubQuestion>
 }
 
 export default function CreateTestPage() {
@@ -20,12 +26,18 @@ export default function CreateTestPage() {
     {
       id: 1,
       name: "Name 1",
-      subQuestions: []
+      subQuestions: [
+        { id: 1, name: "Does", isCorrect: false },
+        { id: 2, name: "Will", isCorrect: true },
+      ]
     },
     {
       id: 2,
       name: "Name 2",
-      subQuestions: []
+      subQuestions: [
+        { id: 1, name: "I", isCorrect: false },
+        { id: 2, name: "He", isCorrect: false },
+      ]
     }
   ]);
 
@@ -42,22 +54,106 @@ export default function CreateTestPage() {
   };
 
   const handleChangeNameQuestion = (
-    id: number, 
+    id: number,
     event: React.ChangeEvent<HTMLInputElement>) => {
-      const questionsCopy = JSON.parse(JSON.stringify(questions));
-      setQuestions(
-        questionsCopy.map((question: Question) => {
-          if (question.id === id) {
-            return {
-              ...question,
-              name: event.target.value
-            }
-          } else {
-            return { ...question };
+    const questionsCopy = JSON.parse(JSON.stringify(questions));
+    setQuestions(
+      questionsCopy.map((question: Question) => {
+        if (question.id === id) {
+          return {
+            ...question,
+            name: event.target.value
           }
-        })
-      ); 
+        } else {
+          return { ...question };
+        }
+      })
+    );
   };
+
+  const handleChangeNameSubQuestion = (
+    questionId: number,
+    subQuestionId: number,
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const questionsCopy = JSON.parse(JSON.stringify(questions));
+    const newQuestions = questionsCopy.map((question: Question) => {
+      if (question.id === questionId) {
+        return {
+          ...question,
+          subQuestions: question.subQuestions.map((subQuestion: SubQuestion) => {
+            if (subQuestion.id === subQuestionId) {
+              return {
+                ...subQuestion,
+                name: event.target.value
+              }
+            } else {
+              return { ...subQuestion }
+            }
+          })
+        }
+      } else {
+        return { ...question }
+      }
+    });
+
+    console.log("New Questions: ", newQuestions);
+    setQuestions(newQuestions);
+  };
+
+  const handleChangeIsCorrect = (
+    questionId: number,
+    subQuestionId: number,
+  ) => {
+    const questionsCopy = JSON.parse(JSON.stringify(questions));
+    const newQuestions = questionsCopy.map((question: Question) => {
+      if (question.id === questionId) {
+        return {
+          ...question,
+          subQuestions: question.subQuestions.map((subQuestion: SubQuestion) => {
+            if (subQuestion.id === subQuestionId) {
+              return {
+                ...subQuestion,
+                isCorrect: !subQuestion.isCorrect
+              }
+            } else {
+              return { ...subQuestion, isCorrect: false }
+            }
+          })
+        }
+      } else {
+        return { ...question }
+      }
+    });
+
+    setQuestions(newQuestions);
+  };
+
+  const addSubQuestion = (questionId: number) => {
+    const questionsCopy = JSON.parse(JSON.stringify(questions));
+    const newQuestions = questionsCopy.map((question: Question) => {
+      if (question.id === questionId) {
+        return {
+          ...question,
+          subQuestions: [
+            ...question.subQuestions, 
+            {
+              id: question.subQuestions.length + 1,
+              name: "",
+              isCorrect: false              
+            }]
+        }
+      } else {
+        return { ...question };
+      }
+    });
+
+    setQuestions(newQuestions);
+  };
+
+  const addSentence = () => {
+    console.log("Click!");
+  }
 
   console.log("questions: ", questions);
 
@@ -96,25 +192,49 @@ export default function CreateTestPage() {
           />
         </div>
         <div>
-          <p>Questions: </p>
+          <p className={styles.nameOfItem}>Questions: </p>
           <div>
-            <p>1 word</p>
-            <div>
-              {questions.map((item) => (
-                <div key={item.id}>
-                  <p>Name: </p>
+            {questions.map((item) => (
+              <div key={item.id} className={styles.subItemContainer}>
+                <div className={styles.nameOfQuestion}>
+                  <p className={styles.nameOfSubItem}>Name: </p>
                   <Input
                     value={item.name}
                     handleChange={(event) => handleChangeNameQuestion(item.id, event)}
-                    label="Correct Answer"
+                    label="Word"
                     size="l"
                     fullWidth={true}
                   />
                 </div>
-              ))}
-            </div>
+                <p className={styles.nameOfSubItem}>Subquestions: </p>
+                {item.subQuestions.map((subQuestion) => {
+                  return (
+                    <div key={subQuestion.id} className={styles.subQuestionContainer}>
+                      <p className={styles.nameOfSubquestion}>Name of Subquestion: </p>
+                      <Input
+                        value={subQuestion.name}
+                        handleChange={(event) => handleChangeNameSubQuestion(
+                          item.id,
+                          subQuestion.id,
+                          event
+                        )}
+                      />
+                      <Checkbox
+                        span="Is Correct Answer"
+                        checked={subQuestion.isCorrect}
+                        onChange={() => handleChangeIsCorrect(item.id, subQuestion.id)}
+                      />
+                    </div>
+                  );
+                })}
+                <div className={styles.addSubquestion}>
+                  <Button size="xs" onclick={() => addSubQuestion(item.id)}>Add Subquestion</Button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
+        <Button size="s" onclick={addSentence}>Add Sentence</Button>
       </div>
     </div>
   );
